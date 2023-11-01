cac49e203e63a19f3fa9c20a866565998e4aeb3d
Norayr <asoyannorayr99@gmail.com>
2023-11-01 19:53:49 +0300

    add

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,12 +1,18 @@
-import java.util.Arrays;
-
 public class Numbers {
     public static void main(String[] args) {
-        int[] array1 = array1 = new int[]{0,1,2,3,4,5,6,7,7};
-        System.out.println(Arrays.toString(array1));
+        int[] array1 = new int[]{0,1,2,3,4,5,6,7,7};
+        printArray(array1);
     }
     public static void printArray(int[] numbers) {
-        int[] array1;
-        array1 = new int[]{0,1,2,3,4,5,6,7,7};
+        System.out.println("[");
+        for (int a = 0; a < numbers.length; a++) {
+            if (a < numbers.length - 1)
+
+                System.out.println(numbers[a] + ",");
+        else {
+                System.out.println(numbers[a]);
+            }
+        }
+        System.out.println("]");
     }
 }
